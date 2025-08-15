83cd427b92329c493c722da52cfaf2adf7450dc0
Adeniyi Peter <adeniyipeteroluwatosin@gmail.com>
2025-08-15 17:02:12 +0100

    panel 3

--- app/Http/Controllers/web/FormsController.php
+++ app/Http/Controllers/web/FormsController.php
@@ -350,6 +350,20 @@ class FormsController extends Controller
                                 ->with('success', "A link to the session has been sent to your email.");
             }
 
+             elseif ($request->resource_type === 'video1') {
+
+                try {
+                    Mail::to($resource_lead->email)->send(
+                        new VideoResourceLead($resource_lead)
+                    );
+                } catch (\Exception $e) {
+                    Log::error("Email sending failed: " . $e->getMessage());
+                }
+
+                return redirect()->route('video.resources.success')
+                                ->with('success', "A link to the session has been sent to your email.");
+            }
+
             return back()->with('danger', 'Invalid request type.')->withInput();
         } catch (ValidationException $th) {
             return back()->with('danger', $th->validator->errors()->first())->withInput();
