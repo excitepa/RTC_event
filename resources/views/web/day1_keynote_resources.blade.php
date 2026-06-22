606d1c2e9f4cfbf9cdba00fb12744b3f8baf3ba9
Peter Adeniyi <adeniyipeteroluwatosin@gmail.com>
2026-06-22 16:09:38 +0100

    day1

--- resources/views/web/day1_keynote_resources.blade.php
+++ resources/views/web/day1_keynote_resources.blade.php
@@ -79,7 +79,7 @@
                 <div class="news-block col-lg-12 col-md-12 col-sm-12 wow fadeInRight">
                     <div class="inner-box">
                         <div class="image-box">
-                            <figure class="image"><a href=""><img src="/public/images/resource/day2Keynote.png" alt=""></a></figure>
+                            <figure class="image"><a href=""><img src="/public/images/resource/day1Keynote.png" alt=""></a></figure>
                         </div>
                         <div class="lower-content">
                             
